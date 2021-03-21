d4fb45bc9827baf491cc3824b79e91077bf71af9
Eduardo <eduardo-balbo@hotmail.com>
2021-03-20 23:23:43 -0300

    first commit - full project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer, Model } from 'miragejs'
+import { App } from './App'
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Criação de Script - Discord Bot',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 13000,
+          createdAt: new Date('2021-02-02 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'GTX 3070 - Rig Mineração',
+          type: 'withdraw',
+          category: 'Compras - Eletrônicos',
+          amount: 8000,
+          createdAt: new Date('2021-02-05 19:00:00')
+        },
+        {
+          id: 3,
+          title: 'Fonte Corsair CX650 - Rig Mineração',
+          type: 'withdraw',
+          category: 'Compras - Eletrônicos',
+          amount: 600,
+          createdAt: new Date('2021-02-05 19:10:00')
+        },
+      ],
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +60,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
